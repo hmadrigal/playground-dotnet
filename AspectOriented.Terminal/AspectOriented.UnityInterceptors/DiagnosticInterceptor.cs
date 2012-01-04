be98f6aa94f6d3b68cc07e898d36e57c60c0c0b3
hmadrigal <hfmad@hotmail.com>
2012-01-03 19:32:43 -0600

    reformating code

--- AspectOriented.Terminal/AspectOriented.UnityInterceptors/DiagnosticInterceptor.cs
+++ AspectOriented.Terminal/AspectOriented.UnityInterceptors/DiagnosticInterceptor.cs
@@ -4,47 +4,52 @@ using Microsoft.Practices.Unity.InterceptionExtension;
 
 namespace AspectOriented.UnityInterceptors
 {
-	public class DiagnosticInterceptor : IInterceptionBehavior
-	{
-		public DiagnosticInterceptor ()
-		{
-		}
+    public class DiagnosticInterceptor : IInterceptionBehavior
+    {
+        public DiagnosticInterceptor()
+        {
+        }
 
-		#region IInterceptionBehavior implementation
-		public IMethodReturn Invoke (IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
-		{
-			System.Console.WriteLine (String.Format ("[{0}:{1}]", this.GetType ().Name, "Invoke"));
-			
-			// BEFORE the target method execution 
-			System.Console.WriteLine (String.Format ("{0}", input.MethodBase.ToString ()));
-			
-			// Yield to the next module in the pipeline
-			var methodReturn = getNext ().Invoke (input, getNext);
-			
-			// AFTER the target method execution 
-			if (methodReturn.Exception == null) {
-				System.Console.WriteLine (String.Format ("Successfully finished {0}", input.MethodBase.ToString ()));
-			} else {
-				System.Console.WriteLine (String.Format ("Finished {0} with exception {1}: {2}", input.MethodBase.ToString (), methodReturn.Exception.GetType ().Name, methodReturn.Exception.Message));
-			}
-			
-			
-			return methodReturn;
-		}
+        #region IInterceptionBehavior implementation
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            System.Console.WriteLine(String.Format("[{0}:{1}]", this.GetType().Name, "Invoke"));
 
-		public IEnumerable<Type> GetRequiredInterfaces ()
-		{
-			System.Console.WriteLine (String.Format ("[{0}:{1}]", this.GetType ().Name, "GetRequiredInterfaces"));
-			return Type.EmptyTypes;
-		}
+            // BEFORE the target method execution 
+            System.Console.WriteLine(String.Format("{0}", input.MethodBase.ToString()));
 
-		public bool WillExecute {
-			get {
-				System.Console.WriteLine (String.Format ("[{0}:{1}]", this.GetType ().Name, "WillExecute"));
-				return true;
-			}
-		}
-		#endregion
-	}
+            // Yield to the next module in the pipeline
+            var methodReturn = getNext().Invoke(input, getNext);
+
+            // AFTER the target method execution 
+            if (methodReturn.Exception == null)
+            {
+                System.Console.WriteLine(String.Format("Successfully finished {0}", input.MethodBase.ToString()));
+            }
+            else
+            {
+                System.Console.WriteLine(String.Format("Finished {0} with exception {1}: {2}", input.MethodBase.ToString(), methodReturn.Exception.GetType().Name, methodReturn.Exception.Message));
+            }
+
+
+            return methodReturn;
+        }
+
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            System.Console.WriteLine(String.Format("[{0}:{1}]", this.GetType().Name, "GetRequiredInterfaces"));
+            return Type.EmptyTypes;
+        }
+
+        public bool WillExecute
+        {
+            get
+            {
+                System.Console.WriteLine(String.Format("[{0}:{1}]", this.GetType().Name, "WillExecute"));
+                return true;
+            }
+        }
+        #endregion
+    }
 }
 
