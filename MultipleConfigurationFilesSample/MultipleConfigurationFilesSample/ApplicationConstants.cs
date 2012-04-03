0d4bd236bfd83289f2bdcc0520ae19097ee7a9a7
hmadrigal <hfmad@hotmail.com>
2012-04-02 22:10:19 -0600

    Adding example of how to maintain multiple configuration files into the same solution

---
+++ MultipleConfigurationFilesSample/MultipleConfigurationFilesSample/ApplicationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace MultipleConfigurationFilesSample
+{
+    public static class ApplicationConstants
+    {
+        public class AppSettings
+        {
+            public const string ServerSharedPath = @"ServerSharedPath";
+            public const string EnvironmentName = @"EnvironmentName";
+            public const string ApplicationDatabase = @"ApplicationDatabase";
+        }
+    }
+}
---
+++ MultipleConfigurationFilesSample/MultipleConfigurationFilesSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+
+namespace MultipleConfigurationFilesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var environment = ConfigurationManager.AppSettings[ApplicationConstants.AppSettings.EnvironmentName];
+            var serverSharedPath = ConfigurationManager.AppSettings[ApplicationConstants.AppSettings.ServerSharedPath];
+            Console.WriteLine(@"Environment: {0} ServerSharedPath:{1}", environment, serverSharedPath);
+            Console.ReadKey();
+        }
+    }
+}
