cc003f3d6b93555c16b634e8d79ed39dfa69dafa
Herberth Madrigal <hfmad@hotmail.com>
2012-08-28 09:09:48 -0600

    Added sample of how to reference a static field in Unity configuration file

---
+++ StaticInstanceTypeConverterSample/EntLibUnity.UnitySample.ConsoleApp/IVersionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntLibUnity.Infrastructure
+{
+    public interface IVersionManager
+    {
+        string Version { get; }
+    }
+}
---
+++ StaticInstanceTypeConverterSample/EntLibUnity.UnitySample.ConsoleApp/VersionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntLibUnity.Infrastructure;
+
+namespace EntLibUnity.UnitySample
+{
+    public class VersionManager : IVersionManager
+    {
+        private readonly Version _version;
+
+        public string Version
+        {
+            get { return _version.ToString(); }
+        }
+
+        public VersionManager(Version version)
+        {
+            _version = version;
+        }
+    }
+}
