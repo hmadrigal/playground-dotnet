e79cee50998502fc681145591096b745d48a74b2
hmadrigal <hfmad@hotmail.com>
2017-08-06 16:22:42 -0600

    Upgrating to .NET 4.5, Importing types without Export/Import attributes

---
+++ MsDotNet.PluginWithMEF/MefAddIns.Language.Portuguese/PortugueseLanguage.cs
@@ -0,0 +1,33 @@
+﻿using MefAddIns.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MefAddIns.Language.Portuguese
+{
+    class PortugueseLanguage : ISupportedLanguage
+    {
+        public string Author
+        {
+            get { return @"Fernando Madrigal"; }
+        }
+
+        public string Version
+        {
+            get { return @"PT-BR.1.0.0"; }
+        }
+
+        public string Description
+        {
+            get { return "Nesta lingua é Português"; }
+        }
+
+
+        public string Name
+        {
+            get { return @"Português"; }
+        }
+    }
+}
--- MsDotNet.PluginWithMEF/MefAddIns.Terminal/Main.cs
+++ MsDotNet.PluginWithMEF/MefAddIns.Terminal/Main.cs
@@ -1,24 +1,29 @@
 namespace MefAddIns.Terminal
 {
-	using System.ComponentModel.Composition.Hosting;
+    using System.ComponentModel.Composition.Hosting;
     using System.ComponentModel.Composition;
     using System;
-	
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			var bootStrapper = new Bootstrapper();
+    using System.ComponentModel.Composition.Registration;
 
-            //An aggregate catalog that combines multiple catalogs
-            var catalog = new AggregateCatalog();
-            //Adds all the parts found in same directory where the application is running!
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var bootStrapper = new Bootstrapper();
+
+            //Adds all the parts found in same directory where the application is running!. It loads by default classes which exposes Export/Immport attributes
             var currentPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(MainClass)).Location) ?? "./";
-            catalog.Catalogs.Add(new DirectoryCatalog(currentPath));
+            var directoryCatalog = new DirectoryCatalog(currentPath);
 
-            //Create the CompositionContainer with the parts in the catalog
-            var container = new CompositionContainer(catalog);
-            
+            // Loading Portuguese implementation, which does not have Export or Import Attributes
+            var assemblyCatalog = GetCustomAssemblyCatalog<Extensibility.ISupportedLanguage>(System.IO.Path.Combine(currentPath, @"MefAddIns.Language.Portuguese.dll"));
+
+
+            //Create the CompositionContainer with the parts in the catalog (An aggregate catalog that combines multiple catalogs)
+            var aggregateCatalog = new AggregateCatalog(assemblyCatalog, directoryCatalog);
+            var container = new CompositionContainer(aggregateCatalog);
+
             //Fill the imports of this object
             try
             {
@@ -29,6 +34,8 @@ namespace MefAddIns.Terminal
                 Console.WriteLine(compositionException.ToString());
             }
 
+
+
             //Prints all the languages that were found into the application directory
             var i = 0;
             foreach (var language in bootStrapper.Languages)
@@ -36,8 +43,23 @@ namespace MefAddIns.Terminal
                 Console.WriteLine("[{0}] {1} by {2}.\n\t{3}\n", language.Version, language.Name, language.Author, language.Description);
                 i++;
             }
-            Console.WriteLine("It has been found {0} supported languages",i);
+            Console.WriteLine("It has been found {0} supported languages", i);
             Console.ReadKey();
-		}
-	}
+        }
+
+        public static System.ComponentModel.Composition.Primitives.ComposablePartCatalog GetCustomAssemblyCatalog<TContract>(string assemblyFilePath)
+        {
+            return ComposeRegistrationForAssembly<TContract>(System.Reflection.Assembly.LoadFile(assemblyFilePath));
+        }
+        public static System.ComponentModel.Composition.Primitives.ComposablePartCatalog ComposeRegistrationForAssembly<TContract>(System.Reflection.Assembly assembly)
+        {
+            var registration = new RegistrationBuilder();
+
+            registration.ForTypesDerivedFrom<TContract>()
+                        .Export<TContract>();
+
+            var catalog = new AssemblyCatalog(assembly, registration);
+            return catalog;
+        }
+    }
 }
